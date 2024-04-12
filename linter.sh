9e32e52f18c87a85dec01a8817ac3b7038b8d6ed
Bianca Rossetti <biancaros05@icloud.com>
2024-04-12 02:35:33 +0000

    Revert "Merge branch 'initialSetup' into 'develop'"
    This reverts merge request !2

--- linter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Uses pycodestyle to enforce pep8
-pycodestyle MedicalApp/*.py --ignore=E501 --show-source > lintreport.txt
-
-#Linting jinja
-djlint MedicalApp/templates/ --profile=jinja --ignore "H005, H030, H031" >> lintreport.txt
