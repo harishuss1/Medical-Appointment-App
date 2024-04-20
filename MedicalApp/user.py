b080ea0ea73da430665a74fbec75d309749cad22
Bianca Rossetti <biancaros05@icloud.com>
2024-04-20 17:05:01 -0400

    modifoed user object to add access level

--- MedicalApp/user.py
+++ MedicalApp/user.py
@@ -1,5 +1,5 @@
 class User:
-    def __init__(self, email, password, first_name, last_name, avatar_path=None, id=None):
+    def __init__(self, email, password, first_name, last_name, access_level, avatar_path=None, id=None):
         if not isinstance(email, str):
             raise ValueError("Illegal type for email")
         if not isinstance(password, str):
@@ -8,6 +8,8 @@ class User:
             raise ValueError("Illegal type for first name")
         if not isinstance(last_name, str):
             raise ValueError("Illegal type for last name")
+        if not isinstance(access_level, str):
+            raise ValueError("Illegal type for access level")
         if avatar_path != None and not isinstance(avatar_path, str):
             raise ValueError("Illegal type for avatar path")
         if id != None and not isinstance(id, int):
@@ -18,14 +20,13 @@ class User:
         self.password = password
         self.first_name = first_name
         self.last_name = last_name
+        self.access_level = access_level
         self.avatar_path = avatar_path
 
 
 class MedicalPatient(User):
-    def __init__(self,  weight, email, password, first_name, last_name, dob, blood_type, height, avatar_path=None, id=None):
-        super().__init__(email, password, first_name, last_name, avatar_path, id)
-        if id != None and not isinstance(id, int):
-            raise ValueError("Illegal type for id")
+    def __init__(self,  weight, email, password, first_name, last_name, access_level, dob, blood_type, height, avatar_path=None, id=None):
+        super().__init__(email, password, first_name, last_name, access_level, avatar_path, id)
         if not isinstance(dob, str):
             raise ValueError("Illegal type for dob")
         if not isinstance(blood_type, str):
@@ -34,7 +35,6 @@ class MedicalPatient(User):
             raise ValueError("Illegal type for height")
         if not isinstance(weight, float):
             raise ValueError("Illegal type for weight")
-        self.id = id
         self.dob = dob
         self.blood_type = blood_type
         self.height = height
