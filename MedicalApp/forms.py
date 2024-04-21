ddfedb1d8850520e3897d0359409a43f2e9c1a8d
Haris Hussain <hussainharis170@gmail.com>
2024-04-21 17:27:33 -0400

    added the necessary forms for authentication

--- MedicalApp/forms.py
+++ MedicalApp/forms.py
@@ -1,3 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, EmailField, PasswordField
 from wtforms.validators import DataRequired, NumberRange
+
+class SignupForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+class LoginForm(FlaskForm):
+    email = EmailField('email')
+    password = PasswordField('password')
