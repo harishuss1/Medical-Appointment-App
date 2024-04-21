64ab829382e71a6e9b177028c14b07d670bf4877
Jackson Tran <tan-jackson.tran@dawsoncollege.qc.ca>
2024-04-21 18:40:27 -0400

    moved appointments form in the form file

--- MedicalApp/forms.py
+++ MedicalApp/forms.py
@@ -1,3 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, EmailField, PasswordField
 from wtforms.validators import DataRequired, NumberRange
+
+class AppointmentForm(FlaskForm):
+    id = IntegerField("ID:", validators=[DataRequired()])
+    patient_id = IntegerField("Patient ID:", validators=[DataRequired()])
+    doctor_id= IntegerField("Doctor ID:", validators=[DataRequired()])
+    appointment_time = StringField("Appointment Time:", validators=[DataRequired()])
+    status = StringField("Status:", validators=[DataRequired()])
+    location = StringField("Location:", validators=[DataRequired()])
+    description = StringField("Description:", validators=[DataRequired()])
