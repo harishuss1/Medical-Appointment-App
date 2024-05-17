5c49f87b78c6dd2ba1bdca62ce5d33681d849935
Bianca Rossetti <biancaros05@icloud.com>
2024-05-17 11:20:44 -0400

    continued tests for appointments

--- MedicalApp/appointments_api.py
+++ MedicalApp/appointments_api.py
@@ -143,14 +143,14 @@ def get_appointment_by_id_api(id):
                     if 'doctor_id' in appointment_json:
                         try:
                             doctor_id = int(appointment_json['doctor_id'])
-                            doctor = get_db().get_user_by_id(doctor_id)
-                            if doctor is None:
-                                abort(make_response(jsonify(id="404", description='Doctor does not exist'), 404))
-                            if doctor.access_level != 'STAFF' and doctor.access_level != 'ADMIN':
-                                abort(make_response(jsonify(id="400", description='User provided is not a doctor'), 400))
-                            appointment.doctor = doctor
                         except:
                             abort(make_response(jsonify(id="400", description='Incorrect type for doctor id'), 400))
+                        doctor = get_db().get_user_by_id(doctor_id)
+                        if doctor is None:
+                            abort(make_response(jsonify(id="404", description='Doctor does not exist'), 404))
+                        if doctor.access_level != 'STAFF' and doctor.access_level != 'ADMIN':
+                            abort(make_response(jsonify(id="400", description='User provided is not a doctor'), 400))
+                        appointment.doctor = doctor
                     if 'appointment_time' in appointment_json:
                         time = appointment_json['appointment_time']
                         try:
@@ -170,9 +170,13 @@ def get_appointment_by_id_api(id):
                     if 'status' in appointment_json:
                         try:
                             status = int(appointment_json['status'])
+                            
                             appointment.status = status
                         except:
                             abort(make_response(jsonify(id="400", description='Invalid status has been provided'), 400))
+                        if status not in (0, 1, -1):
+                            abort(make_response(jsonify(id="404", description='The status you have provided is invalid'), 404))
+                            
                     if 'location' in appointment_json:
                         location = get_db().get_medical_room_by_room_number(appointment_json['location'])
                         if location is None:
@@ -183,7 +187,7 @@ def get_appointment_by_id_api(id):
                 resp.headers['Appointment'] = url_for('appointments_api.get_appointment_by_id_api', id=id)
                 return resp
             except IntegrityError as e:
-                abort(make_response(jsonify(id="400", description='The allergie(s) you have provided do not exist'), 400))
+                abort(make_response(jsonify(id="404", description='The room you have provided does not exist'), 404))
             except DatabaseError as e:
                 abort(make_response(jsonify(id="409", description='Something went wrong with our database'), 409))
             except TypeError as e:
--- tests/test_appointment_api_update.py
+++ tests/test_appointment_api_update.py
@@ -49,6 +49,22 @@ class AppointmentsUpdateTestCases(unittest.TestCase):
         
         self.assertEqual(400, result.status_code)
         
+    def test_update_appointment_patient_invalid_doctor(self):
+        
+        updated_appointment_data = {
+            "doctor_id": 1,
+            "appointment_time": "2025-01-01", 
+            "description": "yayyyy" 
+        }
+        
+        json_data = json.dumps(updated_appointment_data)
+        
+        token = "mIzbZLyEzNKW7SP5NAx9eUHUq_w"
+        headers = {'Authorization': f'Bearer {token}'}
+        result = self.client.put(f'/api/appointments/1', data=json_data, headers=headers, content_type='application/json')
+        response_data = json.loads(result.get_data(as_text=True))
+        self.assertEqual(404, result.status_code)
+        
     def test_update_appointment_patient_invalid_id(self):
         
         updated_appointment_data = {
@@ -81,7 +97,7 @@ class AppointmentsUpdateTestCases(unittest.TestCase):
         
         self.assertEqual(400, result.status_code)
         
-    def test_update_appointment_patient_ignores_access_data(self):
+    def test_update_appointment_patient_ignores_excess_data(self):
         
         updated_appointment_data = {
             "doctor_id": 9,  
@@ -104,6 +120,102 @@ class AppointmentsUpdateTestCases(unittest.TestCase):
         self.assertEqual("yayyyy", appointment.description)
         self.assertEqual(0, appointment.status)
         
-    
+    def test_update_appointment_patient_nofields_400(self):
+        
+        updated_appointment_data = {
+        }
+        
+        json_data = json.dumps(updated_appointment_data)
+        
+        token = "mIzbZLyEzNKW7SP5NAx9eUHUq_w"
+        headers = {'Authorization': f'Bearer {token}'}
+        result = self.client.put(f'/api/appointments/1', data=json_data, headers=headers, content_type='application/json')
+        
+        self.assertEqual(400, result.status_code)
+        
+    def test_update_appointment_doctor_ignoreexcessfields_201(self):
+        
+        updated_appointment_data = {
+            "doctor_id": 9,  
+            "appointment_time": "2025-01-01", 
+            "description": "yayyyy",
+            "status": 1,
+            "location": "105"
+            
+        }
+        
+        json_data = json.dumps(updated_appointment_data)
+        
+        token = "ErU49l4Du_LEvsV1AgU9SIllZ1g"
+        headers = {'Authorization': f'Bearer {token}'}
+        result = self.client.put(f'/api/appointments/1', data=json_data, headers=headers, content_type='application/json')
+        
+        self.assertEqual(201, result.status_code)
+        self.assertEqual('1', result.headers['Appointment'].split("/")[-1])
+        appointment = get_db().get_appointment_by_id(1)
+        self.assertEqual("Regular checkup", appointment.description)
+        self.assertEqual("105", appointment.location.room_number)
+        self.assertEqual(1, appointment.status)
+        
+    def test_update_appointment_doctor_roomnotexist_404(self):
+        
+        updated_appointment_data = {
+            "doctor_id": 9,  
+            "appointment_time": "2025-01-01", 
+            "description": "yayyyy",
+            "status": 1,
+            "location": "106"
+            
+        }
+        
+        json_data = json.dumps(updated_appointment_data)
+        
+        token = "ErU49l4Du_LEvsV1AgU9SIllZ1g"
+        headers = {'Authorization': f'Bearer {token}'}
+        result = self.client.put(f'/api/appointments/1', data=json_data, headers=headers, content_type='application/json')
+        # response_data = json.loads(result.get_data(as_text=True))
+        # print(response_data['description'])
+        self.assertEqual(404, result.status_code)
+        
+    def test_update_appointment_doctor_badstatus_404(self):
+        
+        updated_appointment_data = {
+            "doctor_id": 9,  
+            "appointment_time": "2025-01-01", 
+            "description": "yayyyy",
+            "status": -2,
+            "location": "105"
+            
+        }
+        
+        json_data = json.dumps(updated_appointment_data)
+        
+        token = "ErU49l4Du_LEvsV1AgU9SIllZ1g"
+        headers = {'Authorization': f'Bearer {token}'}
+        result = self.client.put(f'/api/appointments/1', data=json_data, headers=headers, content_type='application/json')
+        # response_data = json.loads(result.get_data(as_text=True))
+        # print(response_data['description'])
+        self.assertEqual(404, result.status_code)
+        
+    def test_update_appointment_doctor_badstatustype_400(self):
+        
+        updated_appointment_data = {
+            "doctor_id": 9,  
+            "appointment_time": "2025-01-01", 
+            "description": "yayyyy",
+            "status": "blahh",
+            "location": "105"
+            
+        }
+        
+        json_data = json.dumps(updated_appointment_data)
+        
+        token = "ErU49l4Du_LEvsV1AgU9SIllZ1g"
+        headers = {'Authorization': f'Bearer {token}'}
+        result = self.client.put(f'/api/appointments/1', data=json_data, headers=headers, content_type='application/json')
+        # response_data = json.loads(result.get_data(as_text=True))
+        # print(response_data['description'])
+        self.assertEqual(400, result.status_code)
+        
 if __name__ == '__main__':
     unittest.main()
